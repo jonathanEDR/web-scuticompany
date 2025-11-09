948547496b48914140edb0c13daafedc8a213110
jonathanEDR <jonathan5@gmail.com>
2025-11-09 11:24:34 -0500

    Fix: Remover exportaciones de componentes eliminados ProfileList y ProfileDirectory

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -8,8 +8,6 @@ export { default as ProfileEditor } from './ProfileEditorSimple';
 export { default as ProfileEditorComplete } from './ProfileEditorComplete';
 export { default as PublicProfile } from './PublicProfile';
 export { default as PublicProfileComplete } from './PublicProfileComplete';
-export { default as ProfileList } from './ProfileList';
-export { default as ProfileDirectory } from './ProfileDirectory';
 
 // Componentes auxiliares
 export { default as AvatarUpload } from './AvatarUpload';
